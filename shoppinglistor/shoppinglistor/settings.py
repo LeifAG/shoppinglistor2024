2c2c74e41b2d036d0b277065b9b7a80686eb9486
unknown <leif.hammar@gymnasium.ax>
2024-10-29 10:59:01 +0200

    vercel build

--- shoppinglistor/shoppinglistor/settings.py
+++ shoppinglistor/shoppinglistor/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
 
 # Default primary key field type
